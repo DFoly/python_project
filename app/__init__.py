cbd64dc8c577ef24cca4221bc373cecb3ec1b824
DFoly <dan.foley@ucdconnect.ie>
2016-11-10 17:25:15 +0000

    Added User Login code

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,20 @@
 from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+import os
+from flask_openid import OpenID
+from config import basedir
+
+
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 
 from app import views, models
+
+lm = LoginManager()
+lm.init_app(app)
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
+
+
